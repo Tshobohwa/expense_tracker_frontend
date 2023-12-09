d08efb2bb46f71bd3478ddb5eb11a985d3ddf6d6
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-12-09 22:39:15 +0200

    Create SplashScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import SplashScreen from "./pages/SplashScreen";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SplashScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PageWrapper from "../components/PageWrapper";
+
+const Login = () => {
+  return (
+    <PageWrapper>
+      <div className="w-[100vw] h-[100vh] bg-wallet bg-cover bg-center">
+        <div className="flex justify-between flex-col items-center gap-3 bg-black/60 h-full w-full p-5">
+          <div className="w-full">
+            <h1 className="font-bold text-5xl text-yellow-200 mt-9">
+              myWallet
+            </h1>
+          </div>
+          <p className=" text-green-200 w-[75%]">
+            An easy and simple way to keep track of your expenses.
+          </p>
+          <div className=" w-full flex flex-col gap-3 items-center">
+            <button className=" text-white font-bold text-lg w-full h-[50px] rounded-md bg-green-400 ">
+              Login
+            </button>
+            <p className=" text-red-400">don't have an account?</p>
+            <button className=" text-green-400 font-bold text-lg w-full h-[50px] rounded-md border border-green-400 ">
+              Sign up
+            </button>
+          </div>
+        </div>
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default Login;
