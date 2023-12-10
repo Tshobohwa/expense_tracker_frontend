655b40f153368db780edbcd4ab4a25cd1a9cda9d
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-12-10 18:39:12 +0200

    Fix shadows

--- src/components/ExpenseChart.jsx
+++ src/components/ExpenseChart.jsx
@@ -4,9 +4,9 @@ const ExpenseChart = ({ category }) => {
   return (
     <div className=" w-[20%] m-2 flex flex-col gap-2 items-center">
       <p style={{ color: category.color }}>{`${category.amount} %`}</p>
-      <div className=" w-full h-[300px] flex flex-col justify-end gap-3">
+      <div className=" w-full h-[300px] flex flex-col justify-end items-center gap-3">
         <div
-          className={`w-full rounded-lg bg-[${category.color}] bg-gray-400`}
+          className={`w-[20px] rounded-lg bg-[${category.color}] bg-gray-400`}
           style={{
             height: `${category.amount * 3}px`,
             backgroundColor: category.color,
--- src/components/TransactionsContainer.jsx
+++ src/components/TransactionsContainer.jsx
@@ -3,7 +3,7 @@ import ExpenseTransaction from "./ExpenseTransaction";
 
 const TransactionsContainer = () => {
   return (
-    <div className=" bg-white rounded-t-3xl pb-8 mx-[-8px] px-2 pt-6 flex flex-col items-center">
+    <div className=" bg-white rounded-t-3xl pb-[100px] mx-[-8px] px-2 pt-6 flex flex-col items-center">
       <p className=" text-green-700 font-bold">Transactions</p>
       <ExpenseTransaction />
       <ExpenseTransaction />
