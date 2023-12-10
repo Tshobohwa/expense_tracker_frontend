036160078d2efba6560c4887604d88f6d91c138d
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-12-10 12:36:42 +0200

    Create assets

--- src/components/ExpenseChartContainer.jsx
+++ src/components/ExpenseChartContainer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ExpenseChartContainer = () => {
   return (
-    <div className=" w-full bg-red-950 rounded-md relative">
-      <button className=" h-[30px] rounded-[15px] px-5 bg-red-700 text-white">
+    <div className=" w-full bg-red-950 rounded-md relative p-2 h-[300px]">
+      <button className=" h-[30px] rounded-[15px] px-5 bg-red-700 text-white absolute top-2 right-2">
         new expense
       </button>
     </div>
