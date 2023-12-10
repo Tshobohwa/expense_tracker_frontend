4ceff32fa1d7624254bc5e27d55ba13a85063aa7
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-12-10 08:53:29 +0200

    Add background images

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import PageWrapper from "../components/PageWrapper";
+import wallet from "../../public/wallet-backgound.jpg";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <PageWrapper>
-      <div className="w-[100vw] h-[100dvh] bg-wallet bg-cover bg-center">
-        <div className="flex justify-between flex-col items-center gap-3 bg-black/60 h-full w-full p-5">
+      <div className="w-[100vw] h-[100dvh] bg-cover bg-center bg-black relative">
+        <img
+          src={wallet}
+          alt=""
+          className=" w-full h-full object-cover object-center"
+        />
+        <div className="flex justify-between flex-col items-center gap-3 bg-black/60 h-full w-full p-5 absolute top-0 left-0 right-0 bottom-0">
           <div className="w-full">
             <h1 className="font-bold text-3xl text-yellow-200 mt-9">
               myWallet
