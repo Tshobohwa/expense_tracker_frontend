1186f4e32254e6b8cb8bfd6e4ab91cb05b13633a
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-12-10 09:52:38 +0200

    Start expenses container

---
+++ src/components/ExpenseChartContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExpenseChartContainer = () => {
+  return (
+    <div className=" w-full bg-red-950 rounded-md relative">
+      <button className=" h-[30px] rounded-[15px] px-5 bg-red-700 text-white">
+        new expense
+      </button>
+    </div>
+  );
+};
+
+export default ExpenseChartContainer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import PageWrapper from "../components/PageWrapper";
 import UserContainer from "../components/UserContainer";
 import BalanceContainer from "../components/BalanceContainer";
+import ExpenseChartContainer from "../components/ExpenseChartContainer";
 
 const Home = () => {
   return (
     <PageWrapper>
-      <div className=" p-2 bg-green-200">
+      <div className=" p-2 bg-green-200 flex flex-col gap-3">
         <UserContainer />
         <BalanceContainer />
+        <ExpenseChartContainer />
       </div>
     </PageWrapper>
   );
