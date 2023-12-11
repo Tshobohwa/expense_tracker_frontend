8d1faa5d7c6732e0bccccf2ea720f6f610db104f
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-12-11 16:20:57 +0200

    Fix form margin and Install react spinners

--- src/components/FormsContainer.jsx
+++ src/components/FormsContainer.jsx
@@ -2,7 +2,7 @@ import { exitImg } from "../assets/icons";
 
 const FormsContainer = ({ children }) => {
   return (
-    <div className=" fixed bg-black/75 top-0 bottom-0 left-0 right-0 z-50 h-[100vh] w-[100vw] m-[-8px] mt-[-10px] flex items-center justify-center px-5">
+    <div className=" fixed bg-black/75 top-0 bottom-0 left-0 right-0 z-50 h-[100vh] w-[100vw] flex items-center justify-center px-5">
       <div className=" w-full h-full relative flex items-center justify-center">
         <button className=" font-bold text-2xl text-white absolute right-4 top-4">
           <img src={exitImg} className=" w-[40px] h-[40px]" />
